234da1a70a066c1d5f668ae0e3b9951422d3a66b
Scato Eggen <scato.eggen@gmail.com>
2015-11-13 16:16:47 +0100

    Run tests with webpack

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    entry: {
+        test: './test'
+    },
+    output: {
+        filename: 'build/[name].js'
+    },
+    //devtool: 'source-map',
+    resolve: {
+        extensions: ['', '.ts', '.js'],
+        alias: {
+            'sinon': 'sinon/pkg/sinon'
+        }
+    },
+    module: {
+        loaders: [
+            { test: /\.ts$/, loader: 'ts-loader' },
+            { test: /sinon/, loader: "imports?define=>false,require=>false" }
+        ]
+    }
+};
