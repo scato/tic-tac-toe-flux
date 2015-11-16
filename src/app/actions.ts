fc658270267fd9f5f36f1b015e3a4fbb9df88c7d
Scato Eggen <scato.eggen@gmail.com>
2015-11-16 08:14:33 +0100

    Add PerformMoveAction

--- src/app/actions.ts
+++ src/app/actions.ts
@@ -1,5 +1,6 @@
 export enum ActionType {
     CHOOSE_PLAYER,
+    PERFORM_MOVE,
 }
 
 export class Action {
@@ -17,5 +18,25 @@ export class ChoosePlayerAction extends Action {
         super(ActionType.CHOOSE_PLAYER);
 
         this.name = name;
+    }
+}
+
+export class PerformMoveAction extends Action {
+    public _x: number;
+    public _y: number;
+
+    constructor (x: number, y: number) {
+        super(ActionType.PERFORM_MOVE);
+
+        this._x = x;
+        this._y = y;
+    }
+
+    get x(): number {
+        return this._x;
+    }
+
+    get y(): number {
+        return this._y;
     }
 }
--- test/app/actions.ts
+++ test/app/actions.ts
@@ -1,26 +1,50 @@
 /// <reference path="../../typings/tsd.d.ts" />
 import {expect} from 'chai';
 import {ActionType, Action} from '../../src/app/actions';
-import {ChoosePlayerAction} from '../../src/app/actions';
+import {ChoosePlayerAction, PerformMoveAction} from '../../src/app/actions';
 
 describe('actions', () => {
     describe('ChoosePlayerAction', () => {
-        let action: ChoosePlayerAction;
+        let subject: ChoosePlayerAction;
 
         beforeEach(() => {
-            action = new ChoosePlayerAction('X');
+            subject = new ChoosePlayerAction('X');
         });
 
         it('is an Action', () => {
-            expect(action).to.be.instanceof(Action);
+            expect(subject).to.be.instanceof(Action);
         });
 
         it('has the right type', () => {
-            expect(action.type).to.equal(ActionType.CHOOSE_PLAYER);
+            expect(subject.type).to.equal(ActionType.CHOOSE_PLAYER);
         });
 
         it('has the right name', () => {
-            expect(action.name).to.equal('X');
+            expect(subject.name).to.equal('X');
+        });
+    });
+
+    describe('PerformMoveAction', () => {
+        let subject: PerformMoveAction;
+
+        beforeEach(() => {
+            subject = new PerformMoveAction(1, 2);
+        });
+
+        it('is an Action', () => {
+            expect(subject).to.be.instanceof(Action);
+        });
+
+        it('has the right type', () => {
+            expect(subject.type).to.equal(ActionType.PERFORM_MOVE);
+        });
+
+        it('has an x coordinate', () => {
+            expect(subject.x).to.equal(1);
+        });
+
+        it('has a y coordinate', () => {
+            expect(subject.y).to.equal(2);
         });
     });
 });
